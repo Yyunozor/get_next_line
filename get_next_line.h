fed3a17d9679802b3b7e8efaa8d2c39a70f7ef89
Yyunozor <yyunozor@icloud.com>
2024-10-31 03:30:26 +0100

    plouf

---
+++ get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anpayot <anpayot@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/31 03:21:02 by anpayot           #+#    #+#             */
+/*   Updated: 2024/10/31 03:22:58 by anpayot          ###   ########.ch       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 32
+#endif
+
+char    *get_next_line(int fd);
+char    *ft_strjoin(char const *s1, char const *s2);
+char    *ft_strdup(const char *s1);
+size_t  ft_strlen(const char *s);
+char    *ft_strchr(const char *s, int c);
+
+#endif
